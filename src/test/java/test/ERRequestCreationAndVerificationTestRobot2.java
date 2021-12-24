a3e82aa4f62b59dff80d9fb89df764c76c325c1d
sandeepkumar1234 <54533323+sandeepkumar1234@users.noreply.github.com>
2021-12-24 15:31:09 +0530

    Update ERRequestCreationAndVerificationTestRobot2.java

--- src/test/java/test/ERRequestCreationAndVerificationTestRobot2.java
+++ src/test/java/test/ERRequestCreationAndVerificationTestRobot2.java
@@ -25,8 +25,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.wtt.docker.listener.PropertiesUtility;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class ERRequestCreationAndVerificationTestRobot2 {
 
